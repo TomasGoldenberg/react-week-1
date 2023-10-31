25ca27b46008d0b92c63aa7b8a3a7e769ef79e80
Tomas <tomas@Tomass-MacBook-Pro.local>
2023-10-31 12:52:23 +0100

    nuevo cambio

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const Person = (props) => {
    return (
     <>
       <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
+      <h2>Last Name: nuevo cambios {props.lastName}</h2>
       <h2>Age: {props.age}</h2>
     </>
 )
